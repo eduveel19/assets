8b27af07b574e38b72a82ef9b4b994fc57b988c1
slowdsports <72911530+slowdsports@users.noreply.github.com>
2021-01-15 17:08:49 -0600

    Players

--- js/player/player.js
+++ js/player/player.js
@@ -1,34 +1,30 @@
-window.onload = function() {
-             var player = new Clappr.Player({
-                 source: fuente,
-                 parentId: '#player',
-                 mimeType: 'audio/mpeg',
-                 watermark: "https://i.ibb.co/Tgk2YrC/Logow-1-1.png", 
-                 position: 'top-left',
-                 plugins: [LevelSelector, ChromecastPlugin, ClapprPip.PipButton, ClapprPip.PipPlugin, DashShakaPlayback, Clappr.MediaControl, PlaybackRatePlugin],
-                 events: {onReady: function() {var plugin = this.getPlugin('click_to_pause'); plugin && plugin.disable();},},
-                 height: '100%',
-                 width: '100%',
-                 autoPlay: false,
-                 mute: false,
-                 shakaConfiguration: {
-                 manifest: {retryParameters: {maxAttempts: Infinity}},
-                 streaming: {retryParameters: {maxAttempts: Infinity}},
-                 drm: {retryParameters: {maxAttempts: Infinity}},
-                 },
-                 playbackRateConfig: {
-                 defaultValue: '1.00x',
-                 options: [
-                 {value: '0.10', label: '0.10x'},
-                 {value: '0.25', label: '0.25x'},
-                 {value: '0.50', label: '0.50x'},
-                 {value: '0.75', label: '0.75x'},
-                 {value: '1.00', label: '1.00x'},
-                 {value: '1.25', label: '1.25x'},
-                 {value: '1.50', label: '1.50x'},
-                 {value: '1.75', label: '1.75x'},
-                 {value: '2.00', label: '2.00x'},
-                 ]
-                 },
-             });
-         };
+var playerInstance = jwplayer("aRzklaXf");
+	   playerInstance.setup({
+	   	"file": fuente,
+        "height":"100%",
+        "width":"100%",
+        stretching: "bestfit",
+	   	"image": "#",
+	   	"mediaid": "player",
+        "mute": false,
+	   	"autostart": false,
+			"cast": {
+				"appid": "player",
+				"logo": "https://i.ibb.co/Tgk2YrC/Logow-1-1.png",
+			},
+           "logo": {
+                "file": "https://i.ibb.co/Tgk2YrC/Logow-1-1.png",
+                "link": "http://slowdsports.com",
+                "hide": "false",
+                "position": "top-left"
+            },
+			/*advertising: {
+				client: "googima",
+				schedule: {
+					adbreak1: {
+						offset: "pre",
+						tag: "http://pubads.g.doubleclick.net/gampad/ads?iu=/7021/fcc.forum&sz=640x480&cust_params=kw%3Dblog_67679%2CNews&ad_rule=0&impl=s&gdfp_req=1&env=vp&output=xml_vast2&unviewed_position_start=1&url=%5BREFERRER_URL%5D&correlator=%5BTIMESTAMP%5D",
+					}
+				}
+			},*/
+	   });
