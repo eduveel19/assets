52557299ebaad7d5615ca062cb3e340831963a70
slowdsports <72911530+slowdsports@users.noreply.github.com>
2021-07-04 23:45:08 -0600

    Update daddy.js

--- js/player/daddy.js
+++ js/player/daddy.js
@@ -1,4 +1,4 @@
 var base = "https://daddylive.me/stream2watch/stream-";
 var end = ".php";
 var fuente = base + chann + end;
-document.write(" <iframe  class='ckst' frameborder='0' height=100% width=100% src='" + fuente + "' allowfullscreen='yes' scrolling='no' allowtransparency></iframe>");
+document.write(" <iframe sandbox='allow-same-origin allow-scripts' class='ddly' frameborder='0' height=100% width=100% src='" + fuente + "' allowfullscreen='yes' scrolling='no' allowtransparency></iframe>");
