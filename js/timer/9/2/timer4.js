0381ddfe5b65519fa98235b46c84565f4474c6a5
Alex <slowdsports@gmail.com>
2021-09-02 23:59:16 -0600

    dsa

--- js/timer/9/2/timer4.js
+++ js/timer/9/2/timer4.js
@@ -517,8 +517,6 @@ function d4t430am() {
 
 d4t430am()
 
-var yyyy = 2021; var mm = 07; var dd = 17;
-
 
 function d4t5am() {
     // Set the date we're counting down to
@@ -1118,8 +1116,6 @@ function d4t1030am() {
 }
 
 d4t1030am()
-
-var yyyy = 2021; var mm = 07; var dd = 17;
 
 
 function d4t11am() {
--- js/timer/9/3/timer3.js
+++ js/timer/9/3/timer3.js
@@ -517,8 +517,6 @@ function d3t430am() {
 
 d3t430am()
 
-var yyyy = 2021; var mm = 07; var dd = 17;
-
 
 function d3t5am() {
     // Set the date we're counting down to
@@ -1118,8 +1116,6 @@ function d3t1030am() {
 }
 
 d3t1030am()
-
-var yyyy = 2021; var mm = 07; var dd = 17;
 
 
 function d3t11am() {
