0726bbf1ade5c683321edb6507f701eaf8a90525
Eduardo <eperdomo833@gmail.com>
2021-09-27 12:17:22 -0600

    mbkjbkjh

--- js/timer/10/1/timer3.js
+++ js/timer/10/1/timer3.js
@@ -1,4 +1,4 @@
-var yyyy = 2021; var mm = 09; var dd = 06;
+var yyyy = 2021; var mm = 08; var dd = 29;
 
 var myTime = yyyy+mm+dd;
 var textLive = "<p class='live-text'>En Vivo <i class='fa fa-circle faa-flash animated'></i></p>";
