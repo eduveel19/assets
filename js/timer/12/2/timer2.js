70f2aec1d8a64134dc14d0f30c98dbed9ed86ff7
Eduardo <eperdomo833@gmail.com>
2022-01-17 15:45:38 -0600

    timer 2da semana enero

--- js/timer/12/2/timer2.js
+++ js/timer/12/2/timer2.js
@@ -1,4 +1,4 @@
-var yyyy = 2021; var mm = 08; var dd = 07;
+var yyyy = 2022; var mm = 00; var dd = 18;
 
 var myTime = yyyy+mm+dd;
 var textLive = "<p class='live-text'>En Vivo <i class='fa fa-circle faa-flash animated'></i></p>";
